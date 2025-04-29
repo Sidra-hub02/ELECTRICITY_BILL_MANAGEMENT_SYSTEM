579d41cc4a3db5664bf197c697a4db23058102b5
Sidra-hub02 <sidraimran2830@gmail.com>
2025-04-29 15:38:17 +0000

    Complete CRUD system ready

---
+++ Models/Meter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EBMS.Models;
+
+public partial class Meter
+{
+    public string Meterid { get; set; } = null!;
+
+    public string Customerid { get; set; } = null!;
+
+    public string Meternumber { get; set; } = null!;
+
+    public string? Metertype { get; set; }
+
+    public virtual Customer Customer { get; set; } = null!;
+
+    public virtual ICollection<Reading> Readings { get; set; } = new List<Reading>();
+}
---
+++ Models/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EBMS.Models;
+
+public partial class Payment
+{
+    public string Paymentid { get; set; } = null!;
+
+    public string Billid { get; set; } = null!;
+
+    public DateOnly Paymentdate { get; set; }
+
+    public decimal Amountpaid { get; set; }
+
+    public string? Paymentmethod { get; set; }
+
+    public virtual Bill Bill { get; set; } = null!;
+}
